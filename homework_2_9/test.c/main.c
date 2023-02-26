554fd541886ba3c23f43eff93bcb1d1b476cae74
chouytc <chou_yin@163.com>
2023-02-26 22:28:49 +0800

    猜数字游戏的实现

--- homework_2_9/test.c/main.c
+++ homework_2_9/test.c/main.c
@@ -349,3 +349,66 @@
 //	return 0;
 //}
 
+//猜数字游戏 - 复习
+
+#include <stdlib.h>
+#include <time.h>
+
+void menu()
+{
+	printf("******************\n");
+	printf("******1.play******\n");
+	printf("******0.exit******\n");
+	printf("******************\n");
+}
+
+void game()
+{
+	int key = rand() % 100 + 1;
+	int guess = 0;
+	while (1)
+	{
+		printf("请输入:>");
+		scanf("%d", &guess);
+		if (guess < key)
+		{
+			printf("猜小了！\n");
+		}
+		else if (guess > key)
+		{
+			printf("猜大了！\n");
+		}
+		else
+		{
+			printf("猜对了！\n");
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			//printf("开始猜数游戏\n");
+			game();
+			break;
+		case 0:
+			printf("游戏已退出，请重新运行游戏\n");
+			break;
+		default:
+			printf("输入不合法，请重新输入\n");
+		}
+	} while (input);
+
+	return 0;
+}
+
