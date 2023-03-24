95bbdbf495bacfb8239f673edf754e26bb696bad
chouytc <chou_yin@163.com>
2023-03-24 22:53:45 +0800

    课后作业代码

--- homework_3_18/homework_3_18/test.c
+++ homework_3_18/homework_3_18/test.c
@@ -194,50 +194,231 @@
 
 
 //倒置单词
-void reverse(char* left, char* right)
-{
-	char temp = 0;
-	while (left < right)
-	{
-		temp = *left;
-		*left = *right;
-		*right = temp;
-		left++;
-		right--;
-	}
-}
+//void reverse(char* left, char* right)
+//{
+//	char temp = 0;
+//	while (left < right)
+//	{
+//		temp = *left;
+//		*left = *right;
+//		*right = temp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[100] = { 0 };
+//	gets(arr);
+//	//三步翻转法
+//	//1. 字符串整体翻转法
+//	//.gnijieb ekil I
+//	int len = strlen(arr);
+//	reverse(arr, arr + len - 1);
+//	//2. 每个单词逆序
+//	//beijing. like I
+//	char* start = arr;
+//	while (*start)
+//	{
+//		char* end = start;
+//		while (*end != ' ' && *end != '\0')
+//		{
+//			end++;
+//		}
+//		//逆序一个单词
+//		reverse(start, end - 1);
+//		if (*end == ' ')
+//		{
+//			start = end + 1;
+//		}
+//		else
+//		{
+//			start = end;
+//		}
+//	}
+//	printf("%s\n", arr);
+//
+//	return 0;
+//}
 
-int main()
-{
-	char arr[100] = { 0 };
-	gets(arr);
-	//三步翻转法
-	//1. 字符串整体翻转法
-	//.gnijieb ekil I
-	int len = strlen(arr);
-	reverse(arr, arr + len - 1);
-	//2. 每个单词逆序
-	//beijing. like I
-	char* start = arr;
-	while (*start)
-	{
-		char* end = start;
-		while (*end != ' ' && *end != '\0')
-		{
-			end++;
-		}
-		//逆序一个单词
-		reverse(start, end - 1);
-		if (*end == ' ')
-		{
-			start = end + 1;
-		}
-		else
-		{
-			start = end;
-		}
-	}
-	printf("%s\n", arr);
 
-	return 0;
-}
+//无法通过测试用例
+//#include <string.h>
+//int main()
+//{
+//	char str[101] = { 0 };
+//	gets(str);
+//	int left = 0;
+//	int right = strlen(str) - 1;
+//	while (left < right)
+//	{
+//		char temp = 0;
+//		temp = str[left];
+//		str[left] = str[right];
+//		str[right] = temp;
+//		left++;
+//		right--;
+//	}
+//	puts(str);
+//
+//	return 0;
+//}
+
+
+
+//#include <string.h>
+//
+//void Reverse(char* str)
+//{
+//	char* left = str;
+//	char* right = str + strlen(str) - 1;
+//	while (left < right)
+//	{
+//		char temp = 0;
+//		temp = *left;
+//		*left = *right;
+//		*right = temp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char str[1000] = { 0 };
+//	gets(str);
+//	Reverse(str);
+//	printf("%s\n", str);
+//
+//	return 0;
+//}
+
+//void print(int* arr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(arr + i));
+//	}
+//}
+//
+//void swap_arr(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	int temp = 0;
+//
+//	while (left < right)
+//	{
+//		while ((left < right) && (arr[left] % 2 == 1))
+//		{
+//			left++;
+//		}
+//		while ((left < right) && (arr[right] % 2 == 0))
+//		{
+//			right--;
+//		}
+//		if (left < right)
+//		{
+//			temp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = temp;
+//		}
+//
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	swap_arr(arr, sz);
+//
+//	print(arr, sz);
+//
+//	return 0;
+//}
+
+
+
+//模拟实现库函数strlen
+//int my_strlen(const char* str)
+//{
+//	int len = 0;
+//	while (*str++)
+//	{
+//		len++;
+//	}
+//	return len;
+//}
+//
+//int main()
+//{
+//	const char str[] = "hello bit";
+//	my_strlen(str);
+//
+//	printf("%d", my_strlen(str));
+//
+//	return 0;
+//}
+
+
+
+//模拟实现库函数strcpy
+
+//#include <assert.h>
+//void my_strcpy(char* dest, const char* src)
+//{
+//	assert(src != NULL);
+//	while (*src)
+//	{
+//		*dest++ = *src++;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr1[10] = "hello bit";
+//	char arr2[10] = { 0 };
+//
+//	my_strcpy(arr2, arr1);
+//
+//	printf("%s\n", arr2);
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//    char a[1000] = { 0 };
+//    int i = 0;
+//    for (i = 0; i < 1000; i++)
+//    {
+//        a[i] = -1 - i;
+//    }
+//    printf("%d", strlen(a));
+//    return 0;
+//}
+
+
+//int main()
+//{
+//	unsigned char a = 200;
+//	//11001000
+//	unsigned char b = 100;
+//	//01100100
+//	unsigned char c = 0;
+//	c = a + b;
+//	//100101100
+//	printf("%d %d", a + b, c);
+//	return 0;
+//}
+
+//unsigned int a = 0x1234;
+//unsigned char b = *(unsigned char*)&a;
+////0x1234
+////00 00 12 34
